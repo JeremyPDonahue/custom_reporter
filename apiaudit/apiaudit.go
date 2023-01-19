dc63fec6d6dad943babbd8be2d3b808257bfb687
jeremy.donahue <jeremy.donahue@nordstrom.com>
2023-01-19 13:55:49 -0800

    Fixed aws-okta issue

--- apiaudit/apiaudit.go
+++ apiaudit/apiaudit.go
@@ -1,25 +1,49 @@
 package apiaudit
 
 import (
-	"time"
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
+	"time"
 )
+
+func RefreshFiles(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.Mkdir(dir, os.ModePerm)
+	} else {
+		d, err := os.Open(dir)
+		if err != nil {
+			fmt.Println("Could not open jsonFiles directory")
+			fmt.Println(err)
+		}
+		defer d.Close()
+
+		names, err := d.Readdirnames(-1)
+		if err != nil {
+			fmt.Println("Could not enumerate the contents of jsonFiles")
+			fmt.Println(err)
+		}
+
+		for _, name := range names {
+			err = os.Remove(filepath.Join(dir, name))
+			if err != nil {
+				fmt.Printf("Could not remove %s", name)
+				fmt.Println(err)
+			}
+		}
+	}
+}
 
 //  collectReourceJSON function is used to gather information from our kubernetes clusters in json format.
 //	It takes the path of the kubeconfig file and the resource to be collected as input.
-//	The function first creates a directory named "jsonFiles" if it does not already exist. 
-//	Then it uses the kubectl command to collect information about the specified resource in json format. 
+//	The function first creates a directory named "jsonFiles" if it does not already exist.
+//	Then it uses the kubectl command to collect information about the specified resource in json format.
 //	The output of the command is saved to a file named "<cluster>-<resource>.json" in the jsonFiles directory.
 //	Finally, the function prints a message indicating that the data has been received for the specified cluster and resource.
 func collectReourceJSON(path, resource string) {
 	jsonFiles := "jsonFiles"
-
-	if _, err := os.Stat(jsonFiles); os.IsNotExist(err) {
-		os.Mkdir(jsonFiles, os.ModePerm)
-	}
 
 	cmd := exec.Command("sh", "-c", fmt.Sprintf("kubectl --kubeconfig %s get %s -A -o json | jq -c", path, resource))
 	stdout, err := cmd.Output()
@@ -46,9 +70,9 @@ func collectReourceJSON(path, resource string) {
 }
 
 // IterateOverPaths function takes in a list of paths and a resource as input and performs the following operations:
-// 1. Iterates over each path in the 'paths' slice 
+// 1. Iterates over each path in the 'paths' slice
 // 2. For each path, calls the 'collectReourceJSON' function passing the current path and the resource
-// 3. For each path in the 'paths' slice, it collects the data for the specified resource 
+// 3. For each path in the 'paths' slice, it collects the data for the specified resource
 func IterateOverPaths(paths []string, resource string) {
 
 	for _, path := range paths {
@@ -57,7 +81,7 @@ func IterateOverPaths(paths []string, resource string) {
 
 }
 
-// GetFileList function is used to get a list of files in a specified directory. 
+// GetFileList function is used to get a list of files in a specified directory.
 // It takes in a 'directory' string as input and performs the following operations:
 // 1. Runs a command "ls directory/ | grep nsk" using the exec package
 // 2. Captures the output of the command execution
@@ -73,7 +97,7 @@ func GetFileList(directory string, toGrepFor string) []string {
 // WriteToFile function is used to write to a file with a specific name.
 // It takes in a 'fileName' and a 'header' string as input and performs the following operations:
 // 1. Get the current date
-// 2. Append the date to the fileName 
+// 2. Append the date to the fileName
 // 3. Open or create a new file with the fileName
 // 4. If there is an error opening the file, it will return an error message
 // 5. Append the header + new line to the file
@@ -85,6 +109,11 @@ func WriteToFile(fileName string, header string) string {
 	now := time.Now()
 	year, month, day := now.Date()
 	fileName = fmt.Sprintf("%s_%d_%d_%d.csv", fileName, year, month, day)
+
+	if _, err := os.Stat(fileName); !os.IsNotExist(err) {
+		os.Remove(fileName)
+	}
+
 	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Sprintf("Unable to open %s", fileName)
@@ -110,7 +139,6 @@ func WriteToFile(fileName string, header string) string {
 // 6. Appends the extracted information to the "apiAudit" file in csv format
 func CreateAPIAuditCSV() {
 	filesToParse := GetFileList("jsonFiles/", "nsk")
-
 	header := "\"Cluster\",\"Name\",\"Namespace\",\"Kind\",\"App-ID\",\"Project-ID\",\"API Version\""
 	fileName := WriteToFile("apiAudit", header)
 
@@ -126,5 +154,3 @@ func CreateAPIAuditCSV() {
 
 	}
 }
-
-
