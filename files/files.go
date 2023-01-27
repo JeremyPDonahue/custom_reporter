7ec7ead587c629ef41c3e465c44dc3906355b37a
jeremy.donahue <jeremy.donahue@nordstrom.com>
2023-01-27 07:07:38 -0800

    Pushing so I can get files package to be recognized

--- files/files.go
+++ files/files.go
@@ -1,1 +1,84 @@
 package files
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"os/exec"
+	"reflect"
+	"strings"
+	"time"
+)
+
+// GetFileList function takes in three string arguments: directory, toGrepFor and toExclude.
+// It uses the exec package to execute a shell command that lists all the files in the directory and
+// filters them based on the passed values of toGrepFor and toExclude.
+// It returns a slice of strings representing the filtered list of files in the directory.
+func GetFileList(directory, toGrepFor, toExclude string) []string {
+
+	var cmd *exec.Cmd
+	if toExclude == "" {
+		cmd = exec.Command("sh", "-c", fmt.Sprintf("ls %s/ | grep %s", directory, toGrepFor))
+	}
+	output, _ := cmd.Output()
+	files := strings.Fields(string(output))
+	return files
+}
+
+func WriteToFile(fileName, content string) string {
+	now := time.Now()
+	year, month, day := now.Date()
+	fileName = fmt.Sprintf("%s_%d_%d_%d.csv", fileName, year, month, day)
+	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Sprintf("Unable to open %s", fileName)
+		fmt.Println(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString(content + "\n"); err != nil {
+		fmt.Sprintf("Unable to write to %s", fileName)
+		fmt.Println(err)
+	}
+
+	return fileName
+}
+
+func structsToCSV(structSlice interface{}, fileName string) {
+
+	structType := reflect.TypeOf(structSlice).Elem().Elem()
+	var headers string
+	for i := 0; i < structType.NumField(); i++ {
+		headers += structType.Field(i).Name + ","
+	}
+	headers = headers[:len(headers)-1]
+	fileName = WriteToFile(fileName, headers string)
+
+	data := [][]string{}
+
+	structValue := reflect.ValueOf(structSlice).Elem()
+
+	for i := 0; i < structValue.Len(); i++ {
+		row := []string{}
+
+		for j := 0; j < structType.NumField(); j++ {
+			row = append(row, fmt.Sprint(structValue.Index(i).Field(j).Interface()))
+		}
+	
+		data = append(data, row)
+	}
+
+	file, _ := os.Open(fileName)
+	defer file.Close()
+	writer := csv.NewWriter(file)
+
+
+	writer.Write([]string{headers})
+	writer.WriteAll(data)
+	writer.Flush()
+
+
+
+	
+
+
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
@@ -81,10 +82,12 @@ func apiAudit(resources []string) {
 	}
 
 	for _, resource := range resources {
+		// f := apiaudit.collectReourceJSON()
 
-		apiaudit.IterateOverPaths(filePathsToKube, resource)
+		filtered := apiaudit.FilterForAPI(apiaudit.CollectReourceJSON(filePathsToKube, resource), "v1beta1")
+		files.structsToCSV(filtered)
 	}
-	apiaudit.CreateAPIAuditCSV()
+	// apiaudit.CreateAPIAuditCSV()
 }
 
 // extractLastPath takes in a slice of strings representing file paths and returns a new slice of strings containing the base of each file path.
@@ -100,24 +103,16 @@ func extractLastPath(paths []string) []string {
 }
 
 func checkAWSLogin() {
-	// check if the user is logged in
-	awsOkta := exec.Command("aws-okta", "info")
-
-	awk := exec.Command("awk", "NR==3 {print $NF}")
-
-	awk.Stdin, _ = awsOkta.StdoutPipe()
-
-	awk.Start()
-
-	awsOkta.Run()
-
-	awk.Wait()
-
-	output, _ := awk.Output()
+	cmd := "aws-okta info | awk 'NR==3 {print $NF}'"
+	out, err := exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	// check the output for "logged in"
-	outputString := string(output)
-	if !strings.Contains(outputString, "-") {
+	outputString := string(out)
+	if strings.Contains(outputString, "-") {
 		fmt.Println("Not logged in, running 'aws-okta login'...")
 		// if not logged in, run the login command
 		loginCmd := exec.Command("aws-okta", "login")
